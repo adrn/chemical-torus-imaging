8ec5c8300d7e69667226189ffd4bda75f3269171
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-08-13 14:28:33 -0400

    break out executed tests into test module

---
+++ thriftshop/config.py
@@ -0,0 +1,6 @@
+import astropy.units as u
+
+# Eilers et al. 2019:
+vcirc = 229. * u.km/u.s
+rsun = 8.122 * u.kpc
+fiducial_mdisk = 6.52551e10 * u.Msun
--- thriftshop/potentials.py
+++ thriftshop/potentials.py
@@ -1,16 +1,10 @@
+from astropy.constants import G
 import astropy.units as u
 import numpy as np
 from scipy.optimize import minimize
 import gala.potential as gp
 
-
-potentials = dict()
-
-# vcirc = 229 km/s @ solar circle (Eilers et al. 2019)
-vcirc = 229.
-rsun = 8.122
-fidu_mdisk = 6.52551e10
-potentials['fiducial'] = gp.MilkyWayPotential(disk=dict(m=fidu_mdisk))
+from .config import fiducial_mdisk, rsun, vcirc
 
 
 def get_mw_potential(mdisk):
@@ -22,8 +16,9 @@ def get_mw_potential(mdisk):
         mhalo = np.exp(ln_mhalo)
         tmp_mw = gp.MilkyWayPotential(disk=dict(m=mdisk),
                                       halo=dict(m=mhalo))
-        test_v = tmp_mw.circular_velocity([-rsun, 0, 0]).to_value(u.km/u.s)[0]
-        return (vcirc - test_v) ** 2
+        test_v = tmp_mw.circular_velocity(
+            [-rsun.to_value(u.kpc), 0, 0] * u.kpc).to_value(u.km/u.s)[0]
+        return (vcirc.to_value(u.km/u.s) - test_v) ** 2
 
     minit = potentials['fiducial']['halo'].parameters['m'].to_value(u.Msun)
     res = minimize(objfunc, x0=np.log(minit),
@@ -37,10 +32,46 @@ def get_mw_potential(mdisk):
                                 halo=dict(m=mhalo))
 
 
-for fac in [0.4, 1.6]:
-    potentials[f'{fac:.1f}'] = get_mw_potential(fac * fidu_mdisk)
+def get_equivalent_galpy(potential):
+    from galpy.potential import (
+        MiyamotoNagaiPotential as BovyMiyamotoNagaiPotential,
+        HernquistPotential as BovyHernquistPotential,
+        NFWPotential as BovyNFWPotential)
 
+    ro = rsun
+    vo = vcirc
+
+    bovy_pot = {}
+
+    amp = (G * potential['disk'].parameters['m']).to_value(vo**2 * ro)
+    a = potential['disk'].parameters['a'].to_value(ro)
+    b = potential['disk'].parameters['b'].to_value(ro)
+    bovy_pot['disk'] = BovyMiyamotoNagaiPotential(amp=amp, a=a, b=b,
+                                                  ro=ro, vo=vo)
+
+    amp = (G * potential['bulge'].parameters['m']).to_value(vo**2 * ro) * 2
+    c = potential['bulge'].parameters['c'].to_value(ro)
+    bovy_pot['bulge'] = BovyHernquistPotential(amp=amp, a=c, ro=ro, vo=vo)
+
+    amp = (G * potential['nucleus'].parameters['m']).to_value(vo**2 * ro) * 2
+    c = potential['nucleus'].parameters['c'].to_value(ro)
+    bovy_pot['nucleus'] = BovyHernquistPotential(amp=amp, a=c, ro=ro, vo=vo)
+
+    _m = potential['halo'].parameters['m']
+    amp = (G * _m).to_value(vo**2 * ro)
+    rs = potential['halo'].parameters['r_s'].to_value(ro)
+    bovy_pot['halo'] = BovyNFWPotential(amp=amp, a=rs, ro=ro, vo=vo)
+
+    return list(bovy_pot.values())
+
+
+# Set up Milky Way models
+potentials = dict()
+potentials['fiducial'] = gp.MilkyWayPotential(disk=dict(m=fiducial_mdisk))
+for fac in [0.4, 1.6]:
+    potentials[f'{fac:.1f}'] = get_mw_potential(fac * fiducial_mdisk)
+
+# Define equivalent galpy potentials:
+galpy_potentials = dict()
 for k, p in potentials.items():
-    assert np.isclose(p.circular_velocity([-rsun, 0, 0]*u.kpc)[0],
-                      vcirc * u.km/u.s,
-                      rtol=1e-5)
+    galpy_potentials[k] = get_equivalent_galpy(p)
