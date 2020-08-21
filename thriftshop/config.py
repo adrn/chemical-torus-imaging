1d3b52f753b200f1801e2107e9e3784b6cd98144
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-08-21 09:25:36 -0400

    get paths from config

--- thriftshop/config.py
+++ thriftshop/config.py
@@ -1,6 +1,15 @@
+import pathlib
 import astropy.units as u
 
 # Eilers et al. 2019:
 vcirc = 229. * u.km/u.s
 rsun = 8.122 * u.kpc
 fiducial_mdisk = 6.52551e10 * u.Msun
+
+# Assumes that the package isn't actually installed, but built in place
+pkg_path = pathlib.Path(__file__).resolve().parent.parent
+cache_path = pkg_path / 'cache'
+plot_path = pkg_path / 'plots'
+fig_path = pkg_path / 'tex/figures'
+for p in [plot_path, fig_path, cache_path]:
+    p.mkdir(exist_ok=True)
